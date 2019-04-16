1cafabe738465b600554f86becb44aefb4f7f340
Victor Pothin <victorpothin1@gmail.com>
2019-04-16 18:13:46 -0300

    Update fault_detect.m

--- fault_detect.m
+++ fault_detect.m
@@ -2,6 +2,14 @@ function fault_statistics = fault_detect(modelo)
 
 all_data = modelo.base_completa;
 fault_statistics.base_completa = all_data;
+[COEFF,~,~,~,~] = pca(all_data);
+C = COEFF(:,1:modelo.num_componentes_escolhidas)*COEFF(:,1:modelo.num_componentes_escolhidas)';
+X = C*all_data';
+X = X'; 
+fault_statistics.x= X;
+warning off
+[~,~,~,T2_tudo,~] = pca(X);
+fault_statistics.T2_all = T2_tudo;
+warning on
 
 end
-
