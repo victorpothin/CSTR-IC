145fd8ef10dba18133fc6b9aa845a2316d4e0ec4
Victor Pothin <victorpothin1@gmail.com>
2019-04-16 17:50:31 -0300

    Update model_ic.m

--- model_ic.m
+++ model_ic.m
@@ -5,7 +5,7 @@ model.base_escolhida = base_escolhida;
 model.base_completa = cell2mat(x(4,base_escolhida));
 dadosbruto = normalize(cell2mat(x(4,base_escolhida)));
 dados = dadosbruto(1:195,:);
-[rows,col] = size(dados);
+[rows,~] = size(dados);
 model.tamanho_da_amostra = rows;
 [COEFF,~,~,~,EXPLAINED] = pca(dados);
 model.porcentagem_todasPCAs = EXPLAINED;
@@ -23,11 +23,14 @@ C = COEFF(:,1:componentes)*COEFF(:,1:componentes)';
 DadoFeitoPCA = C*dados';
 DadoFeitoPCA = DadoFeitoPCA'; 
 model.modelo_dos_dadoscomPCA = DadoFeitoPCA; 
+warning off
 [~,~,~,T2_semfalha,~] = pca(DadoFeitoPCA);
 model.T2_modelo = T2_semfalha;
+warning on
 
-
-
-
+%UCL - limite superior da estatistica
+an = componentes;
+nn = length(T2_semfalha);
+F = finv(0.95, componentes,(size(T2_semfalha,1)-componentes));
+model.UCL = ((an*(nn-1)*(nn+1))/(nn*(nn-an)))*F;
 end
-
