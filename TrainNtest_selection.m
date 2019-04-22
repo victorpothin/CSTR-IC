496ecf70353e2a7592bd5e46342ac7955d72b86c
Victor Pothin <victorpothin1@gmail.com>
2019-04-22 17:08:50 -0300

    Update TrainNtest_selection.m

--- TrainNtest_selection.m
+++ TrainNtest_selection.m
@@ -1,6 +1,6 @@
 function [dataTrain,dataTeste] = TrainNtest_selection(datainput,base_selection)
-%Função para separar o que é dado de teste e o que é dado de treinamento
-dataTeste = cell2mat(datainput(4,base_selection));
+%FunÃ§Ã£o para separar o que Ã© dado de teste e o que Ã© dado de treinamento
+dataTeste = normalize(cell2mat(datainput(4,base_selection)));
 dataTrain = dataTeste(1:195,:);
 end
 
