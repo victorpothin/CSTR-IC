bd03d3ae2d7ed94c81d223364b6ca1b83a30a628
Victor Pothin <victorpothin1@gmail.com>
2019-04-22 17:45:52 -0300

    Add files via upload

---
+++ t2NQ.m
@@ -0,0 +1,61 @@
+function [t2f,T2lim, Qlim,rows] = t2NQ(dataTrain,dataTeste, variance)
+% Função para calcular a estatistica t2 com seu limiar e a 
+% estatistica Q com seu limiar
+%T2 , Q, T2lim , Qlim
+
+CovarianceMatrix = cov(dataTrain);
+[u, s, v] = svd(CovarianceMatrix);
+
+ds = diag(s);
+ds = ds/sum(ds);
+
+a = 0;
+for i=1:length(ds)
+    Acumm(i) = sum(ds(1:i));
+    if Acumm(i)>=(variance/100) && a==0 
+    a = i;
+    end
+end
+
+clear ds
+
+T = u(:,1:a);
+P = dataTrain*T;
+Precons = T*P';
+
+n = length(dataTrain);
+alfa = 0.99;
+T2lim = (a*(n-1)*(n+1)/(n*(n-a)))*finv(alfa,a,n-a);
+
+
+ds = diag(s);
+teta1 = sum(ds(a+1:end));
+teta2 = sum(ds(a+1:end).^2);
+teta3 = sum(ds(a+1:end).^3);
+
+h0 = 1 - (2*teta1*teta3)/(3*teta2^2);
+Ca=norminv([0 alfa],0,1);
+Ca=Ca(2);
+Qlim = teta1*((h0*Ca*sqrt(2*teta2)/teta1) + 1 + (teta2*h0*(h0-1))/(teta1^2))^(1/h0);
+
+
+
+s2 = s(1:a, 1:a);
+for i=1:length(dataTeste)
+t2(i)=dataTeste(i,:)*(T*(s2^-1)*T')*dataTeste(i,:)';
+end
+rows = size(t2,2);
+t2f = t2(1);
+ewma = 0.4;
+ii = 0;
+for i=1:length(t2)
+t2f(i+1)=ewma*t2(i)+(1-ewma)*t2f(i);
+    if t2f(i)>=T2lim && ii==0
+        ii = i;
+    end
+end
+
+
+
+end
+
