37f7df831284511bef93b0cf003c2ab8815f2e05
Victor Pothin <victorpothin1@gmail.com>
2020-09-17 09:28:31 -0300

    Update fault_cont.m
    retira empty spaces

--- fault_cont.m
+++ fault_cont.m
@@ -1,13 +1,10 @@
 function [fault] = fault_cont(phi, philim, base_complete)
-
-fault = [];
-for i = 1:size(base_complete,1)
-    if phi(i) >  philim
-         fault = i;
-         break
+    fault = [];
+    for i = 1:size(base_complete,1)
+        if phi(i) >  philim
+             fault = i;
+             break
+        end
     end
+    return 
 end
-return 
-
-end
-
